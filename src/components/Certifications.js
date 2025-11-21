6c3ecc2a83c6a7ba1fe6cb787fa7d4c42740fe7d
Amartya Kumar <110718238+amartyakumar16@users.noreply.github.com>
2025-11-21 18:21:56 +0530

    changes in certification

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -14,6 +14,10 @@ const certifications = [
     title: 'Xe‑Architect Certification – Xebia Academic Alliance (2024)',
     icon: '📜',
   },
+   {
+    title: 'AWS Academy Graduate – Cloud Foundations',
+    icon: '🎓',
+  },
   {
     title: 'First Runner Up – Intra Volleyball Tournament, UPES',
     icon: '🏆',
